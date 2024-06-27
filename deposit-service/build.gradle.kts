3190495f4bcf5f849c889d96c4436324d5d933d1
Dimitar <dstoyanov@Dimitar-Stoyanovs-MacBook-Pro.local>
2024-06-27 15:58:41 +0300

    Update: All modules added

---
+++ deposit-service/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+  java
+  id("org.springframework.boot") version "3.3.0"
+  id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "com.stoyanov.deposit"
+version = "0.0.1-SNAPSHOT"
+
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(21))
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.kafka:spring-kafka")
+  compileOnly("org.projectlombok:lombok")
+  developmentOnly("org.springframework.boot:spring-boot-devtools")
+  developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+  runtimeOnly("org.postgresql:postgresql")
+  annotationProcessor("org.projectlombok:lombok")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.springframework.boot:spring-boot-testcontainers")
+  testImplementation("org.springframework.kafka:spring-kafka-test")
+  testImplementation("org.springframework.security:spring-security-test")
+  testImplementation("org.testcontainers:junit-jupiter")
+  testImplementation("org.testcontainers:kafka")
+  testImplementation("org.testcontainers:postgresql")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+  useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,18 @@
 rootProject.name = "bank-base"
 
 val services = listOf(
-    //"account-service",
-    //"backup-service",
-//    "deposit-service",
-//    "investment-service",
-//    "loan-service",
-//    "notification-service",
+    "account-service",
+    "backup-service",
+    "deposit-service",
+    "investment-service",
+    "loan-service",
+    "notification-service",
     "frontend",
     "gateway",
     "profile-service",
     "payment-service",
-//    "support-service",
-//    "transaction-service"
+    "support-service",
+    "transaction-service"
 )
 
 services.forEach { service ->
